aa38ff304bb706aaa31be565d3c5ca2f730a2358
Delgado-dot <delgadocristopher350@gmail.com>
2026-07-20 19:48:05 -0500

    Jugador: retroceso visual del disparo

--- Assets/Scripts/PlayerShooter.cs
+++ Assets/Scripts/PlayerShooter.cs
@@ -12,9 +12,18 @@ public class PlayerShooter : MonoBehaviour
     [Header("Fire Rate")]
     [SerializeField] private float fireCooldown = 0.15f;
 
+    [Header("Recoil")]
+    [SerializeField] private float recoilDistance = 0.15f;
+    [SerializeField] private float recoilReturnSpeed = 8f;
+
     private InputAction attackAction;
     private bool useLeft = true;
     private float lastFireTime;
+
+    private Vector3 leftOrigin;
+    private Vector3 rightOrigin;
+    private float leftRecoil;
+    private float rightRecoil;
 
     private void Awake()
     {
@@ -25,6 +34,15 @@ public class PlayerShooter : MonoBehaviour
             firePointRight = transform.Find("FirePointRight");
 
         attackAction = InputHelper.GetPlayerAction(inputActions, "Attack");
+    }
+
+    private void Start()
+    {
+        if (firePointLeft != null)
+            leftOrigin = firePointLeft.localPosition;
+
+        if (firePointRight != null)
+            rightOrigin = firePointRight.localPosition;
     }
 
     private void OnEnable()
@@ -43,6 +61,11 @@ public class PlayerShooter : MonoBehaviour
             attackAction.performed -= OnAttackPerformed;
             attackAction.Disable();
         }
+    }
+
+    private void Update()
+    {
+        ApplyRecoilReturn();
     }
 
     private void OnAttackPerformed(InputAction.CallbackContext _)
@@ -67,6 +90,30 @@ public class PlayerShooter : MonoBehaviour
 
         Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
 
+        if (useLeft)
+            leftRecoil = -recoilDistance;
+        else
+            rightRecoil = -recoilDistance;
+
         useLeft = !useLeft;
+    }
+
+    private void ApplyRecoilReturn()
+    {
+        if (firePointLeft != null)
+        {
+            leftRecoil = Mathf.Lerp(leftRecoil, 0f, recoilReturnSpeed * Time.deltaTime);
+            Vector3 pos = leftOrigin;
+            pos.z += leftRecoil;
+            firePointLeft.localPosition = pos;
+        }
+
+        if (firePointRight != null)
+        {
+            rightRecoil = Mathf.Lerp(rightRecoil, 0f, recoilReturnSpeed * Time.deltaTime);
+            Vector3 pos = rightOrigin;
+            pos.z += rightRecoil;
+            firePointRight.localPosition = pos;
+        }
     }
 }
